ac93bbe98722423c1393760b03ffa9d7bfa076f3
Jaedson Silva <contacanal798@gmail.com>
2022-06-15 16:28:16 -0300

    adding code to generate encoding and decoding dictionary

---
+++ gen_dict.py
@@ -0,0 +1,20 @@
+import string
+from random import choice
+
+characters = string.ascii_letters + string.digits
+full_characters = string.ascii_letters + string.digits + string.punctuation + string.whitespace
+
+def gen():
+    dict_char = {}
+
+    for c in full_characters:
+        chars = ''.join([choice(characters) for __  in range(2)])
+        while chars in dict_char.values():
+            chars = ''.join([choice(characters) for __  in range(2)])
+
+        dict_char[c] = chars
+
+    print(f'characters = {dict_char}')
+
+
+gen()
